e3c98fea15a13164b43daa4c44442dde45cc923a
Qin Huaiqiang <1099280742@qq.com>
2018-05-17 17:32:18 +0800

    Update untar.sh

--- loadshell/untar.sh
+++ loadshell/untar.sh
@@ -6,7 +6,7 @@ node3_ip="$3"
 password="$4"
 cd /root/SDH3.1/
 #rpm包所在的地方也可写成另外一个参数
-tar  xzf ./XData_V3.1.0_b2_24377.tar.gz
+tar  xzf ./XData*
 mv   ./rpm  ./sdhpkg/
 echo "$node1_ip" >  ./sdhpkg/sugon/conf/nodeslist
 echo "$node2_ip" >> ./sdhpkg/sugon/conf/nodeslist
